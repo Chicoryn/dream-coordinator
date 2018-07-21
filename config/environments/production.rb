58ba2cfb7f62f8c4e62a609a41dbd8d56c07e0fc
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2018-07-22 01:17:29 +0200

    Limit production log files to 50Mb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  else
+    config.logger    = ActiveSupport::Logger.new(config.paths['log'].first, 1, 50 * 1024 * 1024)
   end
 
   # Do not dump schema after migrations.
